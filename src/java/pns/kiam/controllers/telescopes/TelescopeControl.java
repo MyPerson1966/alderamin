203e63bdae96cee3d2bc13333d2495d158931bd9
PSEVO tochka <PSEVO tochka@polutoshiba1>
2017-10-11 22:42:30 +0300

    Разделена функц. веб и ejb Телескопы

---
+++ src/java/pns/kiam/controllers/telescopes/TelescopeControl.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pns.kiam.controllers.telescopes;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateful;
+import javax.inject.Named;
+import org.primefaces.event.RowEditEvent;
+import pns.kiam.entities.telescopes.Telescope;
+import pns.kiam.sweb.controllers.telescope.TelescopeController;
+
+/**
+ *
+ * @author PSEVO tochka
+ */
+@Stateful
+@Named
+public class TelescopeControl {
+
+    @EJB
+    private TelescopeController controller;
+
+    public TelescopeController getController() {
+	return controller;
+    }
+
+    public void setController(TelescopeController controller) {
+	this.controller = controller;
+    }
+
+    public void prepareCreation() {
+	controller.prepareCreation();
+    }
+
+    public void rowDeSelect() {
+	controller.rowDeSelect();
+    }
+
+    /**
+     * Editing Table's row
+     *
+     * @param event
+     */
+    public void onRowEdit(RowEditEvent event) {
+	controller.onRowEdit(event);
+    }
+
+    /**
+     * Removes the record(s) from Telescope table. if the "all " parameter is
+     * true, removes all records else it is false removes only selected record
+     * The represenation data in the lisr remove correspondently
+     *
+     * @param all
+     */
+    public void removeRow(boolean all) {
+	controller.removeRow(all);
+    }
+
+    /**
+     * Cancelling edit a row. Setting up a selection as null
+     *
+     * @param event
+     */
+    public void onRowCancel(RowEditEvent event) {
+	controller.onRowCancel(event);
+    }
+
+    /**
+     * Row Select action
+     */
+    public void rowSelect() {
+	controller.rowSelect();
+    }
+
+    /**
+     * Row Select action by given Telescope
+     *
+     * @param t
+     */
+    public void rowSelectAction(Telescope t) {
+	controller.rowSelectAction(t);
+    }
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    public void businessMethod() {
+	System.out.println("businessMethod   21212 jjhjh yfiyfif6");
+    }
+}
---
+++ src/java/pns/kiam/controllers/telescopes/TelescopeMaskControl.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pns.kiam.controllers.telescopes;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.inject.Named;
+import org.primefaces.event.RowEditEvent;
+import org.primefaces.event.SelectEvent;
+import pns.kiam.entities.telescopes.TelescopeHorizontMask;
+import pns.kiam.sweb.controllers.telescope.TelescopeMaskController;
+
+/**
+ *
+ * @author PSEVO tochka
+ */
+@Stateless
+@Named
+public class TelescopeMaskControl {
+
+    @EJB
+    private TelescopeMaskController controller;
+
+    public TelescopeMaskController getController() {
+	return controller;
+    }
+
+    public void setController(TelescopeMaskController controller) {
+	this.controller = controller;
+    }
+
+    public void prepareCreation() {
+	controller.prepareCreation();
+    }
+
+    public void rowDeSelect() {
+	controller.rowDeSelect();
+    }
+
+    /**
+     * Editing Table's row
+     *
+     * @param event
+     */
+    public void onRowEdit(RowEditEvent event) {
+	controller.onRowEdit(event);
+    }
+
+    /**
+     * Removes the record(s) from Telescope table. if the "all " parameter is
+     * true, removes all records else it is false removes only selected record
+     * The represenation data in the lisr remove correspondently
+     *
+     * @param all
+     */
+    public void removeRow(boolean all) {
+	controller.removeRow(all);
+    }
+
+    public void rowSelect(SelectEvent event) {
+	controller.rowSelect(event);
+    }
+
+    public void rowSelectAction(TelescopeHorizontMask t) {
+	controller.rowSelectAction(t);
+    }
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
