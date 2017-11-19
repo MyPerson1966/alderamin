4f4a4094c6a36e77ae211a57e1bcbbd0cd81e8ec
PSEVO tochka <PSEVO tochka@polutoshiba1>
2017-11-19 15:21:53 +0300

    19-11-17  Refresh Users

--- src/java/pns/kiam/controllers/app/ConfigControl.java
+++ src/java/pns/kiam/controllers/app/ConfigControl.java
@@ -6,29 +6,18 @@
 package pns.kiam.controllers.app;
 
 import java.io.IOException;
-import java.io.Serializable;
-import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
-import javax.ejb.Singleton;
-import javax.ejb.Startup;
-import javax.ejb.Stateful;
 import javax.ejb.Stateless;
-import javax.enterprise.context.SessionScoped;
-import javax.faces.application.Application;
-import javax.faces.application.NavigationHandler;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.xml.parsers.ParserConfigurationException;
 import org.xml.sax.SAXException;
-import pns.FileActor;
 import pns.kiam.controllers.users.UserLoginControl;
 import pns.kiam.sweb.controllers.app.XXParserSWEB;
-import pns.kiam.sweb.controllers.satelites.FileMeasuredController;
-import pns.xmlUtils.SXParser;
 
 /**
  *
@@ -56,7 +45,7 @@ public class ConfigControl {
      * @return the value of configAdmPassword
      */
     public String getConfigAdmPassword() {
-        return configAdmPassword;
+	return configAdmPassword;
     }
 
     /**
@@ -65,46 +54,46 @@ public class ConfigControl {
      * @return the value of configADMLogin
      */
     public String getConfigADMLogin() {
-        return configADMLogin;
+	return configADMLogin;
     }
 
     public XXParserSWEB getXxparser() {
-        return xxparser;
+	return xxparser;
     }
 
     @PostConstruct
     public void init() {
 
-        xxparser.setDocUrl("appsconf/s-web.xml");
+	xxparser.setDocUrl("appsconf/s-web.xml");
 
 //        System.out.println("   " + xxparser.getDocUrl());
-        try {
-            xxparser.build();
-            configADMLogin = xxparser.getLogin();
-            configAdmPassword = xxparser.getPassword();
-            archivePath = xxparser.getArchivePath();
-            System.out.println("   -------->> " + archivePath + "   " + xxparser.getArchivePath());
-            xxparser.getFileMeasuredController().readArchiveFileDir();
+	try {
+	    xxparser.build();
+	    configADMLogin = xxparser.getLogin();
+	    configAdmPassword = xxparser.getPassword();
+	    archivePath = xxparser.getArchivePath();
+	    System.out.println("   -------->> " + archivePath + "   " + xxparser.getArchivePath());
+	    xxparser.getFileMeasuredController().readArchiveFileDir();
 
-        } catch (ParserConfigurationException ex) {
-            Logger.getLogger(ConfigControl.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SAXException ex) {
-            Logger.getLogger(ConfigControl.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(ConfigControl.class.getName()).log(Level.SEVERE, null, ex);
-        }
+	} catch (ParserConfigurationException ex) {
+	    Logger.getLogger(ConfigControl.class.getName()).log(Level.SEVERE, null, ex);
+	} catch (SAXException ex) {
+	    Logger.getLogger(ConfigControl.class.getName()).log(Level.SEVERE, null, ex);
+	} catch (IOException ex) {
+	    Logger.getLogger(ConfigControl.class.getName()).log(Level.SEVERE, null, ex);
+	}
     }
 
     /**
      * Killing and stop a session
      */
     public void sessionKill() {
-        System.out.println("  xxparser.getSsessionControl(): " + (xxparser.getSsessionControl() == null) + "   "
-                + xxparser.getSsessionControl().getSession().getId());
-        if (xxparser.getSsessionControl().getSession() != null) {
-            System.out.println("Session to kill: " + xxparser.getSsessionControl().getSession().getId());
-            xxparser.getSsessionControl().sessionDestroy();
-        }
+	System.out.println("  xxparser.getSsessionControl(): " + (xxparser.getSsessionControl() == null) + "   "
+		+ xxparser.getSsessionControl().getSession().getId());
+	if (xxparser.getSsessionControl().getSession() != null) {
+	    System.out.println("Session to kill: " + xxparser.getSsessionControl().getSession().getId());
+	    xxparser.getSsessionControl().sessionDestroy();
+	}
     }
 
 //    /**
@@ -127,8 +116,8 @@ public class ConfigControl {
 //
 //    }
     public void navigate(String gotoURL) {
-        System.out.println("   gotoURL  " + gotoURL);
-        FacesContext facesContext = FacesContext.getCurrentInstance();
-        //NavigationHandler myNav = facesContext.getApplication().getNavigationHandler();
+	System.out.println("   gotoURL  " + gotoURL);
+	FacesContext facesContext = FacesContext.getCurrentInstance();
+	//NavigationHandler myNav = facesContext.getApplication().getNavigationHandler();
     }
 }
